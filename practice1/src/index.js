e801bc32ed33ad598a68ed9d652a0b97ed417a69
wallison da silva gregorio <wallisongregorio2022@gmail.com>
2024-08-26 12:04:00 -0300

    pacotes e comandos a serem utilizados

--- practice1/src/index.js
+++ practice1/src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const app =  express();
 
+//middleware para ler json
+app.use( express.urlencoded( {extended:true} ) )
+app.use(express.json())
 
 app.get('/', (req, res)=> {
     res.send("vamos la")
 })
 
-app.listen(3000, ()=> {
-    console.log("app rodando")
-})
+//connect db
+mongoose.connect(`mongodb+srv://wallisongregorio2022:EbCvSOUAKnV0fEvn@cluster0.f4him.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+    console.log("conectado com sucesso")
+
+    //porta de acesso 
+    app.listen(3000)
+}).catch((err) => console.log(err))
+
