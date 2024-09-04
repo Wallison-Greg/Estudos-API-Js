563f788462caee36efd152aa9697dc5c85315cc1
wallison da silva gregorio <wallisongregorio2022@gmail.com>
2024-09-04 15:04:30 -0300

    instalando pacotes e conectando com o banco

--- practice3/src/index.js
+++ practice3/src/index.js
@@ -1,5 +1,11 @@
+//dotenv
+require("dotenv").config();
+
+//pacotes
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -12,4 +18,8 @@ app.get("/", (req, res) => {
     res.send("start")
 })
 
-app.listen(3000)
+//conexão db
+mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.ir4jc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {app.listen(3000, console.log("conectado"))})
+    .catch((err) => console.log(err))
+
